5dee66a834866d2b4885f92569cff66f1d984313
Pietro Lubrano <pietrolubrano86@gmail.com>
2022-08-26 16:56:46 +0200

    cycling articoli to articoli card

--- src/app/components/articoli-card/articoli-card.component.ts
+++ src/app/components/articoli-card/articoli-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IArticoli } from 'src/app/models/Articoli';
 
 @Component({
   selector: 'app-articoli-card',
@@ -8,6 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class ArticoliCardComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  articolo: IArticoli = {
+    code: '',
+    description: '',
+    um: '',
+    pzcart: 0,
+    peso: 0,
+    prezzo: 0,
+    active: true,
+    data: new Date(),
+    imageUrl: ''
+  }
 
   ngOnInit(): void {
   }
